b0e85a1c9821e762124c5c9cef9e98a6f34563f2
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-01-20 11:56:56 +0530

    Financial & marketing section styling work has been done.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Programs from "./components/Programs";
 import Facilities from "./components/Facilities";
 import Technology from "./components/Technology";
 import Faculty from "./components/Faculty";
+import FinancialModel from "./components/Financial";
+import MarketingOutreach from "./components/Marketing";
 
 function App () {
   return (
@@ -21,6 +23,8 @@ function App () {
       <Facilities />
       <Faculty />
       <Technology />
+      <FinancialModel />
+      <MarketingOutreach />
     </div>
   )
 };
