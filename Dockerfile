79b4ec9c3107b49cdbfdc1750782c9fe3692e751
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-02-13 15:08:44 +0530

    Dockerfile & viteConfig base , build config added for deployment.

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,20 @@
 FROM node:20-alpine AS build
 WORKDIR /app
 
+# Copy package.json and install deps
 COPY package*.json ./
 RUN npm install
 
+# Copy full project
 COPY . .
+
+# Build Vite app
 RUN npm run build
 
 # ---- Stage 2: Serve with Nginx ----
 FROM nginx:alpine
 
-# Copy build output
+# Copy built output into the subfolder
 COPY --from=build /app/dist /usr/share/nginx/html/Psg-leap-academy
 
 EXPOSE 8082
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/Psg-leap-academy/",
+  build: {
+    outDir: "dist",
+  },
   server: {
-    allowedHosts: ['localhost',"leapacademy@psginstitutions.in"]
-  }
-})
+    allowedHosts: ["localhost", "leapacademy@psginstitutions.in"],
+  },
+});
